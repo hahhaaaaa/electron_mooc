c6cb436de692237b8351619b37fec87ccf3721af
xiaofeiyan <1961533510@qq.com>
2020-03-08 22:45:31 +0800

    electron-bootstrap

--- main.js
+++ main.js
@@ -1,93 +1,65 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow,ipcMain} = require('electron')
+const { app, BrowserWindow, ipcMain, Menu } = require('electron')
 const path = require('path')
 class AppWindow extends BrowserWindow {
   constructor(config, fileLocation) {
     const basicConfig = {
-      width: 800,
-      height: 600,
+      width: 1366,
+      height: 760,
+      frame:false,
+      transparent:true,
       webPreferences: {
         nodeIntegration: true
-      }
-    }
+      } 
+    } 
     const finalConfig = { ...basicConfig, ...config }
     super(finalConfig)
     this.loadFile(fileLocation)
     this.once('ready-to-show', () => {
-      this.show()
-    })
+      this.show() 
+    }) 
   }
 }
-// function createWindow () {
-//   // Create the browser window.
-//   const mainWindow = new BrowserWindow({
-//     width: 800,
-//     height: 600,
-//     webPreferences: {
-//       nodeIntegration: true,
-//       // preload: path.join(__dirname, 'preload.js')
-//     }
-//   })
-//   // and load the index.html of the app.
-//   mainWindow.loadFile('./renderer/index.html')
-//   mainWindow.webContents.openDevTools();
-//   // Open the DevTools.
-//   // mainWindow.webContents.openDevTools()
-  
-//   ipcMain.on('add-music-window', function(event, arg) {
-//     const addWindow = new BrowserWindow({
-//           width:800,
-//           height:600,
-//           webPreferences: {
-//             nodeIntegration: true
-//           },
-//           parent: mainWindow
-//         })
-//         addWindow.loadFile('./renderer/add.html')
-//   });
-// }
-// ipcMain.on('add-muisc-window',()=>{
-//   const addWindow = new BrowserWindow({
-//     width:800,
-//     height:600,
-//     webPreferences: {
-//       nodeIntegration: true
-//     },
-//     parent: mainWindow
-//   })
-//   addWindow.loadFile('./renderer/add.html')
-// })
-
-
+//单一实例
+const gotTheLock = app.requestSingleInstanceLock()
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instace', ((event) => {
+     if(mainWIndow.isMinmized()) mainWindow.restore()
+     mainWindow.focus()
+  })) 
+} 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', ()=>{
-  const mainWindow = new AppWindow({},'./renderer/index.html')
+app.on('ready', () => {
+  const mainWindow = new AppWindow({}, './renderer/index.html')
   mainWindow.webContents.openDevTools();
+   
+  Menu.setApplicationMenu(null)
   // mainWindow.loadFile('./render/indx')
-  ipcMain.on('add-music-window',()=>{
-    const addWindow = new AppWindow({
-      width:500,
-      height:400,
-      parent:mainWindow,
-    },'./renderer/add.html')
-  })
+  // ipcMain.on('add-music-window',()=>{
+  //   const addWindow = new AppWindow({
+  //     width:500,
+  //     height:400,
+  //     parent:mainWindow,
+  //   },'./renderer/add.html')
+  // })
   // addWindow.loadFile('./renderer/add.html')
 })
- 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
-}) 
-
+})
+ 
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+}) 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
   
--- renderer/index.js
+++ renderer/index.js
@@ -1,4 +1,134 @@
-const { ipcRenderer } = require('electron')
-document.getElementById('add-music-button').addEventListener('click', () => {
-  ipcRenderer.send('add-music-window');
-})
+// const { ipcRenderer, dialog } = require('electron').remote
+// document.getElementById('add-music-button').addEventListener('click', () => {
+//   ipcRenderer.send('add-music-window');
+// })
+const fs = require('fs')
+const path = require('path')
+const parseString = require('xml2js').parseString
+
+let addSecrete = {
+  config: {
+    secreteId: $('.secreteId'),
+    // 授权文件
+    lincese: $('#button-addon2'),
+
+    addsercretID: $('.addsercretID'),
+
+    cuurentXML: '',
+
+    currentValue: '',
+
+    dogauthArgs: []
+  },
+  init_event () {
+    this.config.lincese.click(() => {
+      this.showOpenDialogs()
+    })
+    this.renderTable(this.config.dogauthArgs)
+
+    this.config.addsercretID.click(() => {
+      console.log('123')
+    })
+  
+  },
+  showOpenDialogs () {
+    const { dialog } = require('electron').remote
+    let options = {
+      title: "打开文件",
+      defaultPath: "",
+      properties: ['openFile'],
+      filters: [
+        { name: 'XML', extensions: ['xml'] },
+      ]
+    }
+
+    dialog.showOpenDialog(options).then(result => {
+      // console.log(result.filePaths[0])
+      if(result.canceled){
+        
+         return 
+      }
+      this.XmlRender(result.filePaths[0]).then(res => {
+        console.log(res)
+      })
+    }).catch(err => {
+      console.err(err)
+    })
+  },
+  XmlRender (filePath) {
+    return new Promise((resolve, reject) => {
+      let data = fs.readFileSync(filePath)
+      parseString(data, (err, result) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
+    })
+    //  await pareseStrig(filePath,(err,result)=>{
+    //      if(err){
+    //        throw err
+    //      }
+    //   })
+  },
+  renderOpertion (value, row, index) {
+     return `<a>删除</a>`
+  },
+  renderTable (data) {
+    $('#table').bootstrapTable('destroy').bootstrapTable({
+      showLoading:false,
+      height:200,
+      theadClasses: "thead-light",//这里设置表头样式
+      classes:"table table-borderd table-striped",
+      columns: [
+        {
+        field: 'id',
+        title: '密码狗ID',
+        }, {
+            field: 'file',
+            title: '授权文件'
+        }, {
+            field: 'operate',
+            title: '操作',
+            width: 120,
+            align: 'center',
+            valign: 'middle',
+            formatter: this.renderOpertion,
+        } 
+      ], 
+      data: data
+    })
+    .bootstrapTable('hideLoading')
+    .bootstrapTable('refreshOptions', {
+      locale:'zh-CN'
+    })
+  },
+ 
+}
+
+// addSecrete.renderTable(this.addSecrete.config.dogauthArgs)
+addSecrete.init_event()
+
+// [{
+//   'id': 0,
+//   'file': undefined,
+  
+// },
+// {
+//   'id': 1,
+//   'file': 'Item 1',
+  
+// },
+// {
+//   'id': 2,
+//   'file': 'Item 2',
+ 
+// },
+// {
+//   'id': 3,
+//   'file': undefined,
+
+// }]
+// addSecrete.renderTable()
+
